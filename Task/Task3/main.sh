6002ef8b91bbda4882ff2bc97355ea17c83bc5a9
DuyOnix <dtduy2210@gmail.com>
2022-04-10 23:32:28 +0700

    fisish report

--- Task/Task3/main.sh
+++ Task/Task3/main.sh
@@ -2,26 +2,27 @@
 set -e
 cd ../../Labsetup/Files
 
-file=pic_original.bmp
+file=greenland.bmp
+eog -n ${file} &
 
 # encrypt the picture greenland.bmp as
 
 # ECB
 openssl enc  -aes-128-ecb  -e -in ${file} -out pic_cipher_ecb.bmp -K 00112233445566778889aabbccddeeff
 
-# Reset the header of the encrypted picture to make it openable by picture viewer
+# encrypt to get a viewable picture
 head -c 54 ${file} > header
 tail -c +55 pic_cipher_ecb.bmp > body
-cat header body > pic_output_ecb.bmp # -> success to have a view picture/
+cat header body > pic_output_ecb.bmp # -> success to have a viewable picture
 eog -n pic_output_ecb.bmp &
 
 # CBC
 openssl enc  -aes-128-cbc  -e -in ${file} -out pic_cipher_cbc.bmp -K 00112233445566778889aabbccddeeff -iv 0102030405060708
 
-# Reset the header of the encrypted picture to make it openable by picture viewer
+# encrypt to get a viewable picture
 head -c 54 ${file} > header
 tail -c +55 pic_cipher_cbc.bmp > body
-cat header body > pic_output_cbc.bmp # -> fail to have a view pictures
+cat header body > pic_output_cbc.bmp # -> fail to have a viewable picture
 eog -n pic_output_cbc.bmp &
 
 
--- Task/Task4/main.sh
+++ Task/Task4/main.sh
@@ -6,25 +6,25 @@ echo -n "hello world" > test.txt
 ls -ld test.txt # 11
 
 # ECB
-openssl enc  -aes-128-ecb  -e -in test.txt -out output.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708
-ls -ld output.bin # 16
+openssl enc  -aes-128-ecb  -e -in test.txt -out ecb_cipher.bin -K 00112233445566778889aabbccddeef
+ls -ld ecb_cipher.bin # 16
 
 # CBC
 openssl enc  -aes-128-cbc  -e -in test.txt -out cbc_cipher.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708
 ls -ld cbc_cipher.bin # 16
 
 # CFB
-openssl enc  -aes-128-cfb  -e -in test.txt -out output.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708 
-ls -ld output.bin # 11
+openssl enc  -aes-128-cfb  -e -in test.txt -out cfb_cipher.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708 
+ls -ld cfb_cipher.bin # 11
 
 # OFB
-openssl enc  -aes-128-ofb  -e -in test.txt -out output.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708
-ls -ld output.bin # 11
+openssl enc  -aes-128-ofb  -e -in test.txt -out ofb_cipher.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708
+ls -ld ofb_cipher.bin # 11
 
 # create 3 file to test CBC mode padding
-echo -n "ABCDE" > f1.txt # 5 bytes
-echo -n "ABCDEFGHIJ" > f2.txt # 10 bytes
-echo -n "ABCDEFGHIJKLMNOP" > f3.txt # 16 bytes
+echo -n "ABCDE" > f1.txt # 5 byte
+echo -n "ABCDEFGHIJ" > f2.txt # 10 byte
+echo -n "ABCDEFGHIJKLMNOP" > f3.txt # 16 byte
 
 # f1.txt encrypted with CBC -> p1.bin
 openssl enc  -aes-128-cbc  -e -in f1.txt -out p1.bin -K 00112233445566778889aabbccddeeff -iv 0102030405060708
@@ -43,13 +43,13 @@ ls -ld p3.bin # 32
 
 # p1.bin decrypt with CBC -> p1_decrypt.txt
 openssl enc  -aes-128-cbc  -d -in p1.bin -out p1_decrypt.txt -K 00112233445566778889aabbccddeeff -iv 0102030405060708 -nopad
-hexdump -C p1_decrypt.txt  # 00000000: 41 42 43 44 45 0b 0b 0b 0b 0b 0b 0b 0b 0b 0b 0b  ABCDE...........
+hexdump -C p1_decrypt.txt   # 00000000: 41 42 43 44 45 0b 0b 0b 0b 0b 0b 0b 0b 0b 0b 0b  ABCDE...........
 
 # p2.bin decrypt with CBC -> p2_decrypt.txt
 openssl enc  -aes-128-cbc  -d -in p2.bin -out p2_decrypt.txt -K 00112233445566778889aabbccddeeff -iv 0102030405060708 -nopad
-hexdump -C p2_decrypt.txt  # 00000000  41 42 43 44 45 46 47 48  49 4a 06 06 06 06 06 06  |ABCDEFGHIJ......|
+hexdump -C p2_decrypt.txt   # 00000000  41 42 43 44 45 46 47 48  49 4a 06 06 06 06 06 06  |ABCDEFGHIJ......|
 
 # p3.bin decrypt with CBC -> p3_decrypt.txt
 openssl enc  -aes-128-cbc  -d -in p3.bin -out p3_decrypt.txt -K 00112233445566778889aabbccddeeff -iv 0102030405060708 -nopad
-hexdump -C p3_decrypt.txt  # 00000000  41 42 43 44 45 46 47 48  49 4a 4b 4c 4d 4e 4f 50  |ABCDEFGHIJKLMNOP|
+hexdump -C p3_decrypt.txt   # 00000000  41 42 43 44 45 46 47 48  49 4a 4b 4c 4d 4e 4f 50  |ABCDEFGHIJKLMNOP|
                             # 00000010  10 10 10 10 10 10 10 10  10 10 10 10 10 10 10 10  |................|
